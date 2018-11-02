6f9055654d04857894f145a6f6b3e43b102a203e
Elyes <elyes@elyess-imac.local>
2018-11-02 23:56:56 +0100

    Initial Commit

---
+++ CookMania/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CookMania
+//
+//  Created by Elyes on 11/2/18.
+//  Copyright © 2018 MiniProjet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
