21ce62d14039f518da5374cee6f8df5f3c8f6d04
elyes007 <elyesmansour@gmail.com>
2018-11-03 23:40:12 +0100

    Added Popular CollectionView

--- CookMania/HomeViewController.swift
+++ CookMania/HomeViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UISearchBarDelegate {
+class HomeViewController: UIViewController, UISearchBarDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Recipe", for: indexPath)
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        let imageName = indexPath.row % 2 == 0 ? "1" : "2"
+        imageView.image = UIImage.init(named: imageName)
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
