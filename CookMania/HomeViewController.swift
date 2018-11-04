b59d4e86579347eaea13570c79489c950ba9badf
elyes007 <elyesmansour@gmail.com>
2018-11-05 00:12:05 +0100

    Added: - Popular Recipe List - Segue "toRecipeList"

--- CookMania/HomeViewController.swift
+++ CookMania/HomeViewController.swift
@@ -10,27 +10,45 @@ import UIKit
 
 class HomeViewController: UIViewController, UISearchBarDelegate, UICollectionViewDataSource {
     
+    let popularRecipes = [
+        Recipe(name: "Italian Chicken Cacciatore", image: "cacciatore"),
+        Recipe(name: "Spaghetti Cacio e Pepe", image: "spaghetti"),
+        Recipe(name: "Easy Caponata", image: "caponata"),
+        Recipe(name: "Melanzana alla Parmigiana", image: "melanzana"),
+    ]
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return popularRecipes.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Recipe", for: indexPath)
         let imageView = cell.viewWithTag(1) as! UIImageView
-        let imageName = indexPath.row % 2 == 0 ? "1" : "2"
-        imageView.image = UIImage.init(named: imageName)
+        imageView.image = UIImage.init(named: popularRecipes[indexPath.row].image!)
         return cell
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
     }
 
+    @IBAction func morePopular(_ sender: Any) {
+        performSegue(withIdentifier: "toRecipeList", sender: popularRecipes)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "toRecipeList":
+            let recipeList = sender as! [Recipe]
+            let destinationController = segue.destination as! RecipeListViewController
+            destinationController.recipeList = recipeList
+        default:
+            print()
+        }
+    }
 }
---
+++ CookMania/Recipe.swift
@@ -0,0 +1,20 @@
+//
+//  Recipe.swift
+//  CookMania
+//
+//  Created by Elyes on 11/4/18.
+//  Copyright © 2018 MiniProjet. All rights reserved.
+//
+
+import Foundation
+
+class Recipe{
+    
+    var name: String?
+    var image: String?
+    
+    init(name: String?, image: String?) {
+        self.name = name
+        self.image = image
+    }
+}
