5f95901500b28807212856a2c2fe9d247bcd2d68
Seif Abdennadher <seifabdennadher@Seifs-MacBook-Pro.local>
2018-11-09 00:31:11 +0100

    Fixing User entity

--- CookMania/Entities/User.swift
+++ CookMania/Entities/User.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 class User: NSObject {
-
+    var id: String
+    var email: String
+    var imageUrl: String
+    var firstName: String
+    var lastName: String
+    
+    override public var description: String { return "User: {id: \(id) , email: \(email) , firtname: \(firstName) , lastname: \(lastName) , imageUrl: \(imageUrl) }"}
+    
+    init(id: String, email: String, firstName: String, lastName: String, imageUrl: String) {
+        self.id = id
+        self.email = email
+        self.firstName = firstName
+        self.lastName = lastName
+        self.imageUrl = imageUrl
+    }
 }
