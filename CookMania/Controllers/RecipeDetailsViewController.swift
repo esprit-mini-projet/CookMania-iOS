09b9b0c86d9f8bb1888f22f6083f1222f751d390
Seif Abdennadher <seifabdennadher@Seifs-MacBook-Pro.local>
2018-11-13 22:46:30 +0100

    Added RecipeDetailsView

--- CookMania/Controllers/RecipeDetailsViewController.swift
+++ CookMania/Controllers/RecipeDetailsViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Cosmos
 
 class RecipeDetailsViewController: UIViewController {
-
+    @IBOutlet weak var recipeCoverIV: UIImageView!
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var ratingView: CosmosView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("***************************")
+        let margin = contentView.frame.width * 0.07
+        print(margin)
+        contentView.addConstraint(NSLayoutConstraint(item: recipeNameLabel, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: margin))
+        contentView.addConstraint(NSLayoutConstraint(item: ratingView, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: margin))
         // Do any additional setup after loading the view.
     }
     
