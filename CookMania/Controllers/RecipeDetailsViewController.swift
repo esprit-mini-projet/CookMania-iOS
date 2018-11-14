9d69b35a11942b129c6b60dd3eff8c18b0d504b7
Seif Abdennadher <seifabdennadher@Seifs-MacBook-Pro.local>
2018-11-14 19:27:32 +0100

    Scrollable DetailsView Ingredients TableView with buttons in each ingredient and a button for all ingredients Steps TableView

--- CookMania/Controllers/RecipeDetailsViewController.swift
+++ CookMania/Controllers/RecipeDetailsViewController.swift
@@ -24,15 +24,21 @@ class RecipeDetailsViewController: UIViewController, UITableViewDataSource, UITa
     @IBOutlet weak var stepsLabel: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var stepsTableView: UITableView!
+    @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ingredientsLabel: UILabel!
+    @IBOutlet weak var ingredientsTableView: UITableView!
+    @IBOutlet weak var ingredientsTableViewConstraint: NSLayoutConstraint!
     
     var steps = ["setp1", "sep2", "step3", "setp1", "sep2", "step3", "setp1", "sep2", "step3", "setp1", "sep2", "step3", "setp1", "sep2", "step3", "setp1", "sep2", "step3"]
-    
+    var ingredients = ["Inge 1", "Inge 2", "Inge 1", "Inge 2", "Inge 1", "Inge 2", "Inge 1", "Inge 2", "Inge 1", "Inge 2", "Inge 1", "Inge 2", "Inge 1", "Inge 2"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initMargin()
         ratingView.settings.updateOnTouch = false
+        tableViewHeightConstraint.constant = CGFloat(44 * steps.count)
+        ingredientsTableViewConstraint.constant = CGFloat(44 * ingredients.count)
     }
     
     func initMargin() {
@@ -51,24 +57,55 @@ class RecipeDetailsViewController: UIViewController, UITableViewDataSource, UITa
         
         //Step Label
         contentView.addConstraint(NSLayoutConstraint(item: stepsLabel, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: margin * 1.5))
+        
+        //Ingredients Label
+        contentView.addConstraint(NSLayoutConstraint(item: ingredientsLabel, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: margin * 1.5))
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return steps.count
+        if tableView == stepsTableView {
+            return steps.count
+        }
+        else{
+            return ingredients.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "stepCell")
-        let contentView = cell?.viewWithTag(0)
-        let stepNameLabel = contentView?.viewWithTag(1) as! UILabel
-        let step = steps[indexPath.row]
-        stepNameLabel.text = step
-        return cell!
+        if(tableView == stepsTableView){
+            let step = steps[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "stepCell")
+            let contentView = cell?.viewWithTag(0)
+            let margin = contentView!.frame.width * 0.15
+            let nameLabel = contentView?.viewWithTag(1) as! UILabel
+            contentView!.addConstraint(NSLayoutConstraint(item: nameLabel, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: margin))
+            nameLabel.text = step
+            return cell!
+        }else{
+            let ingredient = ingredients[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ingredientCell")
+            let contentView = cell?.viewWithTag(0)
+            let margin = contentView!.frame.width * 0.15
+            let button = contentView?.viewWithTag(1) as! UIButton
+            button.restorationIdentifier = String(indexPath.row)
+            let nameLabel = contentView?.viewWithTag(2) as! UILabel
+            contentView!.addConstraint(NSLayoutConstraint(item: nameLabel, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: margin))
+            contentView!.addConstraint(NSLayoutConstraint(item: button, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: margin*0.5))
+            nameLabel.text = ingredient
+            return cell!
+        }
+        
     }
     
-    
+    @IBAction func addIngredientClicked(_ sender: Any) {
+        let button = sender as! UIButton
+        let rowNumber = Int(button.restorationIdentifier!)
+        print(rowNumber)
+    }
 
+    @IBAction func addAllIngredients(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
