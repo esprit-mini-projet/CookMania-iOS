e32d1fde75034f530aedf6becd4ce4d59f506227
elyes007 <elyesmansour@gmail.com>
2018-11-17 21:37:37 +0100

    added other collectionviews in home

--- CookMania/Controllers/HomeViewController.swift
+++ CookMania/Controllers/HomeViewController.swift
@@ -13,29 +13,55 @@ import ObjectMapper
 
 class HomeViewController: UIViewController, UISearchBarDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
+    @IBOutlet weak var topRatedCV: UICollectionView!
+    @IBOutlet weak var fastCheapCV: UICollectionView!
+    @IBOutlet weak var healthyCV: UICollectionView!
+    @IBOutlet weak var kidsCV: UICollectionView!
+    @IBOutlet weak var recentCV: UICollectionView!
+    
     var topRated = [Recipe]()
+    var fastCheap = [Recipe]()
+    var healthy = [Recipe]()
+    var kids = [Recipe]()
+    var recent = [Recipe]()
     
     @IBOutlet weak var topRatedCollectionView: UICollectionView!
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.recentCV{
+            print("RecentCV")
+            return 20
+        }
         return topRated.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Recipe", for: indexPath)
         let imageView = cell.viewWithTag(1) as! UIImageView
-        let nameLabel = cell.viewWithTag(2) as! UILabel
         imageView.layer.cornerRadius = 15
         imageView.layer.masksToBounds = true
+        if collectionView == self.recentCV{
+            return cell
+        }
+        let nameLabel = cell.viewWithTag(2) as! UILabel
+        if collectionView != self.topRatedCV{
+            print("Not Top Rated")
+            return cell
+        }
         imageView.af_setImage(withURL: URL(string: Constants.URL.imagesFolder + topRated[indexPath.row].imageUrl!)!)
         nameLabel.text = topRated[indexPath.row].name!
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.size.width * 0.4
-        return CGSize(width: width, height: collectionView.frame.size.height)
+        var width = collectionView.frame.size.width * 0.4
+        var height = collectionView.frame.size.height
+        if collectionView == self.recentCV{
+            width = collectionView.frame.size.width * 0.49
+            height = width
+        }
+        return CGSize(width: width, height: height)
     }
     
 
