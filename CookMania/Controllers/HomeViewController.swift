f1271d6cc3fc3c7d4cfda8bfd2ce4203aa4c3e32
elyes007 <elyesmansour@gmail.com>
2018-11-18 21:55:01 +0100

    completed home look

--- CookMania/Controllers/HomeViewController.swift
+++ CookMania/Controllers/HomeViewController.swift
@@ -11,28 +11,20 @@ import AlamofireImage
 import Alamofire
 import ObjectMapper
 
-class HomeViewController: UIViewController, UISearchBarDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var topRatedCV: UICollectionView!
     @IBOutlet weak var fastCheapCV: UICollectionView!
     @IBOutlet weak var healthyCV: UICollectionView!
     @IBOutlet weak var kidsCV: UICollectionView!
-    @IBOutlet weak var recentCV: UICollectionView!
     
     var topRated = [Recipe]()
     var fastCheap = [Recipe]()
     var healthy = [Recipe]()
     var kids = [Recipe]()
-    var recent = [Recipe]()
-    
-    @IBOutlet weak var topRatedCollectionView: UICollectionView!
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == self.recentCV{
-            print("RecentCV")
-            return 20
-        }
         return topRated.count
     }
     
@@ -41,30 +33,19 @@ class HomeViewController: UIViewController, UISearchBarDelegate, UICollectionVie
         let imageView = cell.viewWithTag(1) as! UIImageView
         imageView.layer.cornerRadius = 15
         imageView.layer.masksToBounds = true
-        if collectionView == self.recentCV{
-            return cell
-        }
+        let url = URL(string: Constants.URL.imagesFolder + topRated[indexPath.row].imageUrl!)!
+        imageView.af_setImage(withURL: url)
         let nameLabel = cell.viewWithTag(2) as! UILabel
-        if collectionView != self.topRatedCV{
-            print("Not Top Rated")
-            return cell
-        }
-        imageView.af_setImage(withURL: URL(string: Constants.URL.imagesFolder + topRated[indexPath.row].imageUrl!)!)
         nameLabel.text = topRated[indexPath.row].name!
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        var width = collectionView.frame.size.width * 0.4
-        var height = collectionView.frame.size.height
-        if collectionView == self.recentCV{
-            width = collectionView.frame.size.width * 0.49
-            height = width
-        }
+        let width = collectionView.frame.size.width * 0.4
+        let height = collectionView.frame.size.height
         return CGSize(width: width, height: height)
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchTopRated()
@@ -95,7 +76,10 @@ class HomeViewController: UIViewController, UISearchBarDelegate, UICollectionVie
     func fetchTopRated(){
         Alamofire.request(Constants.URL.topRatedRecipes).responseString(completionHandler: { (response: DataResponse<String>) in
             self.topRated = Mapper<Recipe>().mapArray(JSONString: response.result.value!)!
-            self.topRatedCollectionView.reloadData()
+            self.topRatedCV.reloadData()
+            self.fastCheapCV.reloadData()
+            self.kidsCV.reloadData()
+            self.healthyCV.reloadData()
         })
     }
 }
